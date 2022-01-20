fd05d3a08264305c6ccb1d946f12c02730c4ce42
shota-nukumizu <author2000.1225@gmail.com>
2022-01-21 08:54:08 +0900

    create Dataset

--- layouts/default.vue
+++ layouts/default.vue
@@ -106,7 +106,12 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
-        }
+        },
+        {
+          icon: 'mdi-account',
+          title: 'Users',
+          to: '/users'
+        },
       ],
       miniVariant: false,
       right: true,
